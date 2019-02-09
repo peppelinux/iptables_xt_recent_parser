1b9dcd321b814350f0d6f723ba25651b86c47e7a
peppelinux <peppelinux@yahoo.it>
2019-02-09 17:08:53 +0100

    0.4

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ def readme():
         return f.read()
 
 setup(name='iptables_xt_recent_parser',
-      version='0.3',
+      version='0.4',
       description="Tool used for converting jiffies from iptables xt_recent into timestamps.",
       long_description=readme(),
       classifiers=['Development Status :: 5 - Production/Stable',
